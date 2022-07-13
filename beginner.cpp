c5b3bc7ceadbc2c06a4348b74a57dc9fe884fd1e
Bhavya Gor <bhavya.gor9999@gmail.com>
2022-07-13 14:23:55 +0530

    first commit

---
+++ beginner.cpp
@@ -0,0 +1,214 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // cout << "hello world" << endl;
+    // int a;
+    // a = 12;
+    // cout << "SIZE OF A:" << sizeof(a) << endl;
+    // float b;
+    // cout << "size of b:" << sizeof(b) << endl;
+    // char c;
+    // cout << "size of c:" << sizeof(c) << endl;
+    // double d;
+    // cout << "size of d:" << sizeof(d) << endl;
+    // bool e;
+    // cout << "size of e:" << sizeof(e) << endl;
+
+    // short int i;
+    // long int j;
+    // cout << "size of i:" << sizeof(i) << endl;
+    // cout << "size of j:" << sizeof(j) << endl;
+
+    // largest of 3 numbers
+    //  int num1, num2, num3;
+    //  cin >> num1 >> num2 >> num3;
+    //  if (num1 > num2)
+    //  {
+    //      if (num1 > num3)
+    //      {
+    //          cout << "num 1 is greatest:" << num1 << endl;
+    //      }
+    //      else
+    //      {
+    //          cout << "num 3 is greatest:" << num3 << endl;
+    //      }
+    //  }
+    //  else
+    //  {
+    //      if (num2 > num3)
+    //      {
+    //          cout << "num 2 is greatest:" << num2 << endl;
+    //      }
+    //      else
+    //      {
+    //          cout << "num 3 is greatest:" << num3 << endl;
+    //      }
+    //  }
+
+    // even odd
+    //  int num;
+    //  cin >> num;
+    //  if (num % 2 == 0)
+    //  {
+    //      cout << "num is even" << endl;
+    //  }
+    //  else
+    //  {
+    //      cout << "num is odd" << endl;
+    //  }
+
+    // looppss
+    // int n;
+    // cin >> n;
+    // int sum = 0;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     sum = sum + i;
+    // }
+    // cout << sum << endl;
+
+    // while loopp
+    // int n;
+    // cin >> n;
+    // while (n > 0)
+    // {
+    //     cout << n << endl;
+    //     cin >> n;
+    // }
+
+    // int n;
+    // cin >> n;
+    // int flag = 0;
+    // for (int i = 2; i <= n / 2; i++)
+    // {
+    //     if (n % i == 0)
+    //     {
+    //         flag = 0;
+    //         break;
+    //     }
+    //     else
+    //     {
+    //         flag = 1;
+    //     }
+    // }
+    // if (flag == 0)
+    // {
+    //     cout << "non prime" << endl;
+    // }
+    // else
+    // {
+    //     cout << "prime" << endl;
+    // }
+    // int pocketMoney = 3000;
+    // for (int date = 1; date <= 30; date++)
+    // {
+    //     if (date % 2 == 0)
+    //     {
+    //         continue;
+    //     }
+    //     if (pocketMoney == 0)
+    //     {
+    //         break;
+    //     }
+    //     cout << "go out today date is: " << date << endl;
+    //     pocketMoney = pocketMoney - 300;
+    // }
+
+    // PRIME NUMBERS
+    //  int n;
+    //  cin >> n;
+    //  int i;
+    //  for (i = 2; i < n; i++)
+    //  {
+    //      if (n % i == 0)
+    //      {
+    //          cout << "Non prime" << endl;
+    //          break;
+    //      }
+    //  }
+    //  if (i == n)
+    //  {
+    //      cout << "prime number " << n << endl;
+    //  }
+
+    // PRINT N prime NUMBERS(in range)
+    // int a, b;
+    // cin >> a >> b;
+    // int i;
+    // for (int num = a; num <= b; num++)
+    // {
+    //     // checking prime number
+    //     for (i = 2; i < num; i++)
+    //     {
+    //         if (num % i == 0)
+    //         {
+    //             break;
+    //         }
+    //     }
+    //     if (i == num)
+    //     {
+    //         cout << num << endl;
+    //     }
+    // }
+
+    // Switch case
+
+    // char button;
+    // cin >> button;
+    // switch (button)
+    // {
+    // case 'A':
+    //     cout << "A" << endl;
+    //     break;
+    // case 'B':
+    //     cout << "B" << endl;
+    //     break;
+    // case 'C':
+    //     cout << "C" << endl;
+    //     break;
+    // default:
+    //     cout << "hello world!" << endl;
+    //     break;
+    // }
+
+    // simple calculator
+
+    // int a, b;
+    // cin >> a >> b;
+    // char choice;
+    // cin >> choice;
+    // switch (choice)
+    // {
+    // case 'a':
+    //     cout << "sum is:" << a + b << endl;
+    //     break;
+    // case 's':
+    //     cout << "minus is:" << a - b << endl;
+    //     break;
+    // case 'm':
+    //     cout << "multiply is:" << a * b << endl;
+    //     break;
+    // case 'd':
+    //     cout << "divide is:" << a / b << endl;
+    //     break;
+    // default:
+    //     cout << "hello!" << endl;
+    // }
+
+    // Bit wise operators (0 and 1) &(and),|(or),^(xor),~(ones complement),<<(left shift 4<<1 shifts 0100 indicating 4 makes it 1000),>>(right shift)
+    //  a << n is equal to a * 2^n
+    //  a >> n is equal to a / 2^n
+    //  int a = 10;
+    //  int b = 5;
+    //  int c = (a>b)? a-b : b-a;
+    // cast one datatype to another
+
+    // int a;
+    // cout << &a;
+    // int *b = &a;
+    // cout << b;
+
+    return 0;
+}
