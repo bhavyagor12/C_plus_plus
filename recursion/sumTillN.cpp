c90badd541cd89ae636c1d79a40d3fae3e5a892e
Bhavya Gor <bhavya.gor9999@gmail.com>
2022-07-18 17:28:57 +0530

    recursion basics

---
+++ recursion/sumTillN.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sum(int n)
+{
+    if (n == 0)
+        return 0;
+    int prevSum = sum(n - 1);
+    return n + prevSum;
+}
+int multiply(int n, int p)
+{
+    if (p == 0)
+    {
+        return 1;
+    }
+    int prevValue = multiply(n, p - 1);
+    return n * prevValue;
+}
+int factorial(int n)
+{
+    if (n == 0 || n == 1 || n == 2)
+        return n;
+    int value = factorial(n - 1);
+    return n * value;
+}
+int fibonnaci(int n)
+{
+
+    if (n == 0 || n == 1)
+    {
+        return n;
+    }
+
+    return fibonnaci(n - 1) + fibonnaci(n - 2);
+}
+int main()
+{
+    int n, p;
+    cin >> n;
+    // cout << sum(n) << endl;
+    // cout << multiply(n, p) << endl;
+    // cout << factorial(n) << endl;
+    cout << fibonnaci(n) << endl;
+}
