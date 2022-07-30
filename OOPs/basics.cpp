0e5c585d791e77c74c96d67053688e53618c2421
Bhavya Gor <bhavya.gor9999@gmail.com>
2022-07-30 12:15:11 +0530

    basics of oops

--- OOPs/basics.cpp
+++ OOPs/basics.cpp
@@ -4,11 +4,38 @@ using namespace std;
 
 class student
 {
+    int sapid;
+
 public:
     string name;
     int age;
     bool gender;
 
+    student()
+    {
+        cout << "this is the default constructor";
+    }
+
+    // parameterized constructor
+    student(string s, int a, bool g)
+    {
+        name = s;
+        age = a;
+        gender = g;
+    }
+
+    // copy constructor
+    student(student &a)
+    {
+        name = a.name;
+        age = a.age;
+        gender = a.gender;
+    }
+
+    void getSapid(int i)
+    {
+        sapid = i;
+    }
     void getInfo()
     {
         cout << "Enter Name: " << endl;
@@ -27,6 +54,20 @@ public:
         cout << "Gender: ";
         cout << gender << endl;
     }
+    // operator overloading
+    bool operator==(student &a)
+    {
+        if (name == a.name && age == a.age && gender == a.gender)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    ~student()
+    {
+        cout << "this is the destructor!";
+    }
 };
 
 int main()
@@ -36,14 +77,31 @@ int main()
     // a.age = 20;
     // a.gender = 0;
 
-    student arr[3];
-    for (int i = 0; i < 3; i++)
-    {
-        arr[i].getInfo();
-    }
+    // student arr[3];
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     int j;
+    //     cout << "Sapid = ";
+    //     cin >> j;
+    //     arr[i].getSapid(j);
+    //     arr[i].getInfo();
+    // }
 
-    for (int i = 0; i < 3; i++)
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     arr[i].printInfo();
+    // }
+
+    student a("bhavya", 20, 0);
+    student c = a;
+
+    // need to do operator overloading
+    if (c == a)
     {
-        arr[i].printInfo();
+        cout << " same af";
+    }
+    else
+    {
+        cout << "different af";
     }
 }
