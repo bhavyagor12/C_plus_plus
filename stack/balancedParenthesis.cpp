cf0e7f9e8a1bc5e7d4c286fe48f6dd4a49cef759
Bhavya Gor <bhavya.gor9999@gmail.com>
2022-08-09 22:10:32 +0530

    hi

--- stack/balancedParenthesis.cpp
+++ stack/balancedParenthesis.cpp
@@ -8,6 +8,7 @@ bool isValid(string s)
     {
         if (s[i] == '{' || s[i] == '[' || s[i] == '(')
         {
+
             st.push(s[i]);
         }
         else if (!st.empty() && s[i] == ')')
