b9931dabdac1ce018dcda7373831375fcd4ad4ec
Bhavya Gor <bhavya.gor9999@gmail.com>
2022-08-10 21:41:31 +0530

    peak element changes

--- Binary Search/peakElement.cpp
+++ Binary Search/peakElement.cpp
@@ -4,40 +4,46 @@ using namespace std;
 int findPeak(vector<int> &nums)
 {
     int size = nums.size();
-    // int peak = 0;
     int low = 0, high = size - 1;
     int prev = 0, next = 0;
+    int mid = 0;
+    if (size == 1)
+    {
+        return 0;
+    }
     while (low <= high)
     {
-        int mid = low + (high - low) / 2;
+        mid = low + (high - low) / 2;
         prev = mid - 1;
         next = mid + 1;
-        if (mid == 0 && nums[mid] > nums[prev])
+        if (mid == 0 && nums[mid] > nums[next])
         {
             return low;
         }
-        if (mid == size - 1 && nums[mid] > nums[high])
+        if (mid == size - 1 && nums[mid] > nums[prev])
         {
             return high;
         }
-        else if (nums[mid] > nums[prev] && nums[mid] > nums[next])
+
+        else if (mid != 0 && mid != size - 1 && nums[mid] > nums[prev] && nums[mid] > nums[next])
         {
             return mid;
         }
-        else if (nums[mid] < nums[prev])
+        else if (mid != 0 && nums[mid] < nums[prev])
         {
             high = mid - 1;
         }
-        else if (nums[mid] < nums[next])
+        else if (mid != size - 1 && nums[mid] < nums[next])
         {
             low = mid + 1;
         }
     }
+    return -1;
 }
 
 int main()
 {
-    vector<int> nums = {10, 20, 30, 40, 50};
+    vector<int> nums = {1, 2};
     cout << findPeak(nums) << endl;
 
     return 0;
